0009bdc285f1f5abaa1cc07d2b6108b4e6aa3fa1
Bahaa Mohamed <bahaaazoz398@gmail.com>
2024-10-05 22:50:08 +0300

    Login & router

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-// register User
-
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -34,8 +33,29 @@ export const register = async (req, res) => {
       impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
+    delete savedUser.password;
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING USER */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    return res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
-// Configrations
+/* CONFIGRATIONS */
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,7 +26,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// file storage
+/* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -37,11 +38,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// route with files
-
+/* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRoutes);
 
-// mongos setup
+/* MOONGOSE SETUP */
 
 const PORT = process.env.PORT || 6001;
 
